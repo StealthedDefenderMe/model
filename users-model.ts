e6ce104fa8c6c3985be1585a0e5d2d99c96e1125
Prathamesh <prathamesh@Prathameshs-MacBook-Air.local>
2025-02-08 16:45:23 +0530

    initial commit

--- users-model.ts
+++ users-model.ts
@@ -1,1 +1,30 @@
-// This is begining of the model submodule in git hub repo
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+
+@Schema({
+    collection: 'users',
+    timestamps: true
+})
+
+export class UsersModel {
+
+    @Prop({ type: String, required: true })
+    firstName: string;
+
+    @Prop({ type: String, required: true })
+    lastName: string;
+
+    @Prop({ type: String, required: true })
+    userName: string;
+
+    @Prop({ type: String, required: true })
+    email: string;
+
+    @Prop({ type: Number, required: true })
+    mobileNumber: number;
+
+}
+
+export type UsersModelDocument = HydratedDocument<UsersModel>
+export const UserSchemas = SchemaFactory.createForClass(UsersModel)
+
